043fd6b1fa5a5e6e24f0141b343fed4e89500847
Basab <basabdatta.gupta@tum.de>
2026-05-17 17:37:22 +0530

    fix formatting

--- simple/cpp/substitute-algorithm_after.cpp
+++ simple/cpp/substitute-algorithm_after.cpp
@@ -4,10 +4,12 @@
 #include <span>
 #include <array>
 
-std::optional<std::string> FoundPerson(std::span<const std::string> people){
+std::optional<std::string> FoundPerson(std::span<const std::string> people)
+{
     static constexpr std::array<std::string_view, 3> candidates{"Don", "John", "Kent"};
 
-    if (const auto it = std::ranges::find_first_of(people, candidates); it != people.end()) {
+    if (const auto it = std::ranges::find_first_of(people, candidates); it != people.end())
+    {
         return *it;
     }
     return std::nullopt;
