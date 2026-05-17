0d869ced500a082e75c3c9d20e7165299024d26f
Basab <basabdatta.gupta@tum.de>
2026-05-17 13:57:02 +0530

    add working code examples for extract method technique in c++26 and use idiomatic language features

---
+++ simple/cpp/extract-method_after.cpp
@@ -0,0 +1,14 @@
+#include <print>
+void printBanner(){}
+
+std::string getOutstanding() { return "42.00"; }
+
+[[nodiscard]] std::string formatDetails(std::string_view name, std::string_view outstanding)
+{
+    return std::format("name: {}\namount: {}\n", name, outstanding);
+}
+
+void printDetails(std::string_view name) {
+    printBanner();
+    std::println("{}", formatDetails(name, getOutstanding()));
+}
---
+++ simple/cpp/extract-method_before.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <format>
+void printBanner(){}
+std::string getOutstanding() { return "42.00"; }
+
+void printDetails(std::string_view name) {
+    printBanner();
+
+    // print details
+    std::cout << std::format("name: {}\n", name);
+    std::cout << std::format("amount: {}\n",  getOutstanding());
+}
